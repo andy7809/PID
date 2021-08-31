6609000016561ad1908ce15401d82a42ced5f245
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2021-08-31 16:38:11 -0500

    Remove default parameters

--- src/pid/pid.py
+++ src/pid/pid.py
@@ -1,5 +1,5 @@
 class PID:
-    def __init__(self, p = 1, i = 1, d = 1, sp = 10, memory = 10, x = 0):
+    def __init__(self, p, i, d, sp, memory, x):
         self.e_hist = []
 
         self.p = p
@@ -30,9 +30,3 @@ class PID:
             u += self.d * (e(self) - self.e_hist[-1]) / dt
         self.e_hist.append(e(self))
         self.x += u * dt
-
-c = PID(p=0.3, i=0.002, d=0.2, memory=1000)
-
-for t in range(50000):
-    c.update()
-    print(c.x)
