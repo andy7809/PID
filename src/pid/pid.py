78415e547f9b901315f02a26979cec108a1f1c94
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2021-08-31 16:34:38 -0500

    Move pid.py to new folder | fix name not found

--- src/pid/pid.py
+++ src/pid/pid.py
@@ -12,26 +12,27 @@ class PID:
 
         self.x = x
 
-    def integrate(self):
-        riemann_sum = 0
-        for e in self.e_hist:
-            riemann_sum += e * dt
-        return riemann_sum
-
-    def e(self):
-        return self.sp - self.x
-
     def update(self, dt = 0.1):
+        def integrate(self):
+            riemann_sum = 0
+            for e in self.e_hist:
+                riemann_sum += e * dt
+            return riemann_sum
+
+        def e(self):
+            return self.sp - self.x
+
         u = 0
-        u += self.p * self.e()
-        u += self.i * self.integrate()
+        u += self.p * e(self)
+        u += self.i * integrate(self)
+
         if self.e_hist:
-            u += self.d * (self.e() - self.e_hist[-1]) / dt
-        self.e_hist.append(self.e())
+            u += self.d * (e(self) - self.e_hist[-1]) / dt
+        self.e_hist.append(e(self))
         self.x += u * dt
 
 c = PID(p=0.3, i=0.002, d=0.2, memory=1000)
 
-for t in range(500):
+for t in range(50000):
     c.update()
     print(c.x)
